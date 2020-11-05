184b9331d7d17708eca0f9079d3d869eb79ac79e
ArturMarekNowak <arturmareknowal@gmail.com>
2020-11-05 22:45:34 +0100

    To do: implement haarcascades and allow img and videos input

--- Raspberry/Detection/detection.py
+++ Raspberry/Detection/detection.py
@@ -1,47 +1,70 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from imutils.object_detection import non_max_suppression
-import numpy as np
-import time
-import cv2
-import imutils
+from sys import argv 
 
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+script, mode = argv
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 16
-rawCapture = PiRGBArray(camera, size=(320, 240))
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-        orig = image.copy()	
+if mode[0] == "c":
+    
+    print("loaded cam")
 
-        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale =1.05)
+    
+    from picamera.array import PiRGBArray
+    from picamera import PiCamera
+    from imutils.object_detection import non_max_suppression
+    import numpy as np
+    import time
+    import cv2
+    import imutils
 
-        for (x, y, w, h) in rects:
-            cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    haar_upper_body_cascade = cv2.CascadeClassifier(r"data/haarcascade_upperbody.xml")
 
-        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    # Uncomment this for real-time webcam detection
+    # If you have more than one webcam & your 1st/original webcam is occupied,
+    # you may increase the parameter to 1 or respectively to detect with other webcams, depending on which one you wanna use.
 
-        for (xA, yA, xB, yB) in pick:
-            cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
-            print("Jest co najmniej jeden kwadrat")
+    video_capture = cv2.VideoCapture(0)
+
+    # For real-time sample video detection
+    #video_capture = cv2.VideoCapture("subway.mp4")
+    video_width = video_capture.get(3)
+    video_height = video_capture.get(4)
+
+    while True:
+        ret, frame = video_capture.read()
+
+        #frame = imutils.resize(frame, width=320) # resize original video for better viewing performance
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert video to grayscale
+
+        upper_body = haar_upper_body_cascade.detectMultiScale(
+            gray,
+            scaleFactor = 1.1,
+            minNeighbors = 5,
+            minSize = (50, 100), # Min size for valid detection, changes according to video size or body size in the video.
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the upper bodies
+        for (x, y, w, h) in upper_body:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1) # creates green color rectangle with a thickness size of 1
+            cv2.putText(frame, "Upper Body Detected", (x + 5, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2) # creates green color text with text size of 0.5 & thickness size of 2
+        cv2.imshow('Video', frame) # Display video
+
+        # stop script when "q" key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+    
+
+else:
+    
+    if mode[-4:] == ".avi":
+        path = "Resources\\" + mode 
+        print "loaded movie file: %s" % path
+    elif mode[-4:] == ".jpg":
+        path = "Resources\\" + mode
+        print "loaded pic file: %s" % path
 
 
-        # show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+
