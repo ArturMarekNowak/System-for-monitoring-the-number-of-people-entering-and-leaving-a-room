e75589f5ec6afc80a14e0fb49a2bed695095c9c5
ArturMarekNowak <arturmareknowal@gmail.com>
2020-11-10 22:05:20 +0100

    added input from pictures and videos

--- Raspberry/Detection/detection.py
+++ Raspberry/Detection/detection.py
@@ -2,6 +2,8 @@ from sys import argv
 
 script, mode = argv
 
+cascadePath = r'HaarCascades/haarcascade_frontalface_default.xml'
+ 
 if mode[0] == "c":
     
     print("loaded cam")
@@ -13,7 +15,6 @@ if mode[0] == "c":
     import time
     from picamera.array import PiRGBArray
     from picamera import PiCamera    
-    cascadePath = r'HaarCascades/haarcascade_frontalface_default.xml'
     camera = PiCamera()
     camera.resolution = (320, 240)
     camera.framerate = 16
@@ -43,136 +44,63 @@ if mode[0] == "c":
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    '''
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 32
-    rawCapture = PiRGBArray(camera, size=(640, 480))
-
-    time.sleep(0.1)
-
-
-
-    face_cascade =  cv2.CascadeClassifier('/home/pi/Downloads/haarcascade_frontalface_default.xml')
-    
-    with picamera.PiCamera() as camera:
-        stream = io.BytesIO()
-    
-        for foo in camera.capture_continuous(stream, format='jpeg'):
-    	
-	    for frame in camera.capture_continuous(rawCapture, format="bgr",  use_video_port=True):
-
-    		img=np.asarray(frame.array)
-    		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-   		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    		for (x,y,w,h) in faces:
-        	    img = cv2.Rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                    
-        	    roi_gray = gray[y:y+h, x:x+w]
-        	    roi_color = img[y:y+h, x:x+w]
-     
-	    # Truncate the stream to the current position (in case
-            # prior iterations output a longer image)
-            stream.truncate()
-            stream.seek(0)
-            if process(stream):
-                break
-
-    '''
-    ''' 
-    from picamera.array import PiRGBArray
-    import picamera
-    from imutils.object_detection import non_max_suppression
-    import numpy as np
-    import time
-    import cv2
-    mport imutils
-    
-    
-    stream = io.BytesIO()
-    
-    with picamera.PiCamera() as camera: 
-        camera.resolution(320, 240)
-        camera.capture(stream, format = 'jpeg')
-
-    buff = np.fromstring(stream.getvalue(), dtype = np.uint8)
-
-    image = cv2.imdecode(buff, 1)
-
-    face_cascade = cv2.CascadeClassifier(r"/home/pi/Inzynierka/Raspberry/HaarCascades")
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2ARRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-
-    print "Found " + str(len(faces)) + " face(s)"
-    
-    for(x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    cv2.imshow("Video", image)
-
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
-
-    '''
-
-
-            #haar_cascade_1 = cv2.CascadeClassifier(r"HaarCascades/haarcascade_upperbody.xml")
-
-    # Uncomment this for real-time webcam detection
-    # If you have more than one webcam & your 1st/original webcam is occupied,
-    # you may increase the parameter to 1 or respectively to detect with other webcams, depending on which one you wanna use.
-
-    '''
-            camera = PiCamera()
-            camera.resolution = (320, 240)
-            camera.framerate = 16
-
-            rawCapture = PiRGBArray(camera, size=(320, 240))
-            time.sleep(0.1)
-
-            for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-
-                img = np.array(frame.array)
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-                casc1 = haar_cascade_1.detectMultiScale(gray, 1.3, 5)
-
-        # Draw a rectangle around the upper bodies
-                for (x, y, w, h) in casc1:
-                    img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1) 
-                    roi_gray = gray[y:y+h, x:x+w]
-                    roi_color = color[y:y+h, x:x+w]
-
-            #camera.truncate(0)
-            #camera.seek(0)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        
-
-            cv2.imshow('Video', img) # Display video
-            stream.truncate()
-            stream.seek(0)
-            
-            if process(stream):
-                break
-        
-        # Release capture
-        video_capture.release()
-        cv2.destroyAllWindows()
-    '''
-
 else:
     
-    if mode[-4:] == ".avi":
-        path = "Resources\\" + mode 
-        print "loaded movie file: %s" % path
+    if mode[-4:] == ".mp4":
+        import cv2
+        import imutils
+        
+        path = "docs/" + mode
+        print "loaded video file: %s" % path
+
+        cap = cv2.VideoCapture(path)
+
+        while True:
+            success, img = cap.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cascade = cv2.CascadeClassifier(cascadePath)
+            faces = cascade.detectMultiScale(gray, 1.3, 5)
+            for(x, y, w, h) in faces:
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = img[y:y+h, x:x+w]
+
+            cv2.imshow("Video", img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        '''
+        img = cv2.imread(path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cascade = cv2.CascadeClassifier(cascadePath)
+        faces = cascade.detectMultiScale(gray, 1.3, 5)
+        for(x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+
+        cv2.imshow('video', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        '''
+
     elif mode[-4:] == ".jpg":
-        path = "Resources\\" + mode
+        import cv2
+        import imutils
+        
+        path = "docs/" + mode
         print "loaded pic file: %s" % path
 
+        img = cv2.imread(path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cascade = cv2.CascadeClassifier(cascadePath)
+        faces = cascade.detectMultiScale(gray, 1.3, 5)
+        for(x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+
+        cv2.imshow('image', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
